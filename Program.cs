4c6412bd7c7b3d3b943d1c307265ffce698f16d4
Wendel <renanwendel100@gmail.com>
2024-07-17 10:56:44 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+namespace ConsumindoApiFake;
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+
+        HttpClient client = new();
+        string url = "https://jsonplaceholder.typicode.com/users/1";
+
+        var response = await client.GetAsync(url);
+        var content = await response.Content.ReadAsStringAsync();
+        var json = JsonConvert.DeserializeObject<Cliente.Welcome>(content);
+        Console.WriteLine(json.Name);
+         
+
+    }
+}
